38a6cbfdaba9489ad6a37609cd1477334466fc93
Thomas Neugschwandtner <ic17b082@technikum-wien.at>
2018-06-11 19:03:15 +0200

    first push

---
+++ common.h
@@ -0,0 +1,38 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+/**
+* @file common.h
+* Betriebssysteme common code for receiver and sender
+* Beispiel 3
+*
+* @author Dominic Schebeck 			<ic17b049@technikum-wien.at>
+* @author Thomas  Neugschwandtner 	<ic17b082@technikum-wien.at>
+* @author Dominik Rychly 			<ic17b052@technikum-wien.at>
+*
+* @date 06/11/2018
+*
+* @version 1.0
+*
+*/
+
+/*
+* -----------------------------------------includes----------------------------------
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif // COMMON_H
+
+//------------------------------------eof--------------------------------------------------
---
+++ receiver.c
@@ -0,0 +1,53 @@
+/**
+* @file receiver.c
+* Betriebssysteme receiver File of the ringbuffer
+* Beispiel 2
+*
+* @author Dominic Schebeck 			<ic17b049@technikum-wien.at>
+* @author Thomas  Neugschwandtner 	<ic17b082@technikum-wien.at>
+* @author Dominik Rychly 			<ic17b052@technikum-wien.at>
+*
+* @date 06/11/2018
+*
+* @version 1.0
+*
+*/
+
+/*
+* -----------------------------------------includes----------------------------------
+*/
+
+
+
+
+
+
+
+
+/**
+* \brief This is the main entry point for any C program
+*
+* \param argc the number of arguments
+* \param argv the arguments itselves (including the program name in argv[0])
+*
+* \return always "success"
+* \retval 0 always
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//------------------------------------eof--------------------------------------------------
